f234db7014c6065a8527142518ef967e5750e289
ppodds <oscar20020629@gmail.com>
2022-07-27 21:33:30 +0800

    Feat: initial commit

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    {{ a }}
+  </div>
+</template>
+<script setup lang="ts">
+const { data: test } = await useFetch("/api/test");
+console.log("1");
+console.log(test.value);
+const a = computed(() => {
+  console.log("2");
+  console.log(test.value);
+  return test.value.sort();
+});
+</script>
